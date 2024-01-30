38648b88c6298aab6f25f308a541e04459ed741e
ALYSSA HOPPLE <ajh7039@psu.edu>
2024-01-29 22:31:05 -0500

    Added comments to see what I did

--- python-nlp/nlpexercise1/nlpexercise1.py
+++ python-nlp/nlpexercise1/nlpexercise1.py
@@ -8,11 +8,13 @@ wordStrings = str(words)
 print(wordStrings)
 
 chatWords = nlp(wordStrings)
-
+# Here is a list of items and character names, as well as the foods that are mentioned as a part of the recipe.
+# It is not perfect, but I am new to Python and spaCy.
 print("Characters and Items")
 for token in chatWords:
     if token.pos_ == "PROPN":
         print(token.text)
-
+# I originally recycled this line of code to experiment with other spaCy commands.
+# I will also use such code for future reference to spaCy. It will take time for me to get used to Python and spaCy.
 for token in chatWords:
     print(token.text, "---->", token.pos_, ":::::", token.lemma_)

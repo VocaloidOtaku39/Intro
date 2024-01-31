e77d7a728ecd983a11b7e07e3e21179c7761d68f
ALYSSA HOPPLE <ajh7039@psu.edu>
2024-01-31 18:42:42 -0500

    I added more fun spaCy commands to my ChatGPT txt!

--- python-nlp/nlpexercise1/nlpexercise1.py
+++ python-nlp/nlpexercise1/nlpexercise1.py
@@ -17,4 +17,10 @@ for token in chatWords:
 # I originally recycled this line of code to experiment with other spaCy commands.
 # I will also use such code for future reference to spaCy. It will take time for me to get used to Python and spaCy.
 for token in chatWords:
-    print(token.text, "---->", token.pos_, ":::::", token.lemma_)
+    print(token.text, ":", token.pos_, ". This word's root is", token.lemma_)
+
+for ent in chatWords.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+for token in chatWords:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_)

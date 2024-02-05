5a844f131020a55a9be5b4eb315b876465204e22
ALYSSA HOPPLE <ajh7039@psu.edu>
2024-02-05 17:01:59 -0500

    I made changes to Exercrise 2

--- python-nlp/nlpexercise1/nlpexercise2.py
+++ python-nlp/nlpexercise1/nlpexercise2.py
@@ -11,65 +11,70 @@ insideDir = os.listdir(workingDir)
 print("inside this directory are the following files AND directories: " + str(insideDir))
 
 # use os.path.join to connect the subdirectory to the working directory:
-CollPath = os.path.join(workingDir, 'textCollection')
-print(CollPath)
+CollPath = os.path.join(workingDir, 'nlpexercise2')
+print(f'{CollPath=}')
 
-chatResults = open('ahopple-chatgpt-results.txt', 'r', encoding="utf8")
-otherResults = open('nlpexercise2/ahopple-claude-results.txt')
-words = chatResults.read()
-wordStrings = str(words)
-newWords = otherResults.read()
-newStrings = str(newWords)
-print(wordStrings)
+def readTextFiles(filepath):
+    with open(filepath, 'r', encoding='utf8') as f:
+        chatresults = open('ahopple-chatgpt-results.txt', 'r', encoding="utf8")
+        otherresults = open('nlpexercise2/ahopple-claude-results.txt')
+        words = chatresults.read()
+        wordstrings = str(words)
+        newwords = otherresults.read()
+        newstrings = str(newwords)
+        print(wordstrings)
 
-chatWords = nlp(wordStrings)
-claude = nlp(newStrings)
-# Here is a list of items and character names, as well as the foods that are mentioned as a part of the recipe.
-# It is not perfect, but I am new to Python and spaCy.
-print("Characters and Items")
-for token in chatWords:
-    if token.pos_ == "PROPN":
-        print(token.text)
-# I originally recycled this line of code to experiment with other spaCy commands.
-# I will also use such code for future reference to spaCy. It will take time for me to get used to Python and spaCy.
-for token in chatWords:
-    print(token.text, ":", token.pos_, ". This word's root is", token.lemma_)
+        chatwords = nlp(wordstrings)
+        claude = nlp(newstrings)
 
-for ent in chatWords.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+        vectors = chatwords.vector
 
-for token in chatWords:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_)
+        wordofinterest = nlp(u'Samus')
+        print(wordofinterest, ' : ', wordofinterest.vector_norm)
 
-for token in claude:
-    if token.pos_ == "PROPN":
-        print(token.text)
+        wordofinterest2 = nlp(u'Ridley')
+        print(wordofinterest2, ' : ', wordofinterest2.vector_norm)
 
-vectors = chatWords.vector
+        wordOfInterest3 = nlp(u'Metroid')
+        print(wordOfInterest3, ' : ', wordOfInterest3.vector_norm)
 
-wordOfInterest = nlp(u'Samus')
-print(wordOfInterest, ' : ', wordOfInterest.vector_norm)
-
-wordOfInterest2 = nlp(u'Ridley')
-print(wordOfInterest2, ' : ', wordOfInterest2.vector_norm)
-
-wordOfInterest3 = nlp(u'Metroid')
-print(wordOfInterest3, ' : ', wordOfInterest3.vector_norm)
-
-highSimilarityDict = {}
-for token in chatWords:
-    if (token and token.vector_norm):
-        # if token not in highSimilarityDict.keys(): Alas, this did not work to remove duplicates from my dictionary
-        if wordOfInterest.similarity(token) > .3:
-            highSimilarityDict[token] = wordOfInterest.similarity(token)
+        highSimilarityDict = {}
+        for token in chatwords:
+            if (token and token.vector_norm):
+                if wordofinterest.similarity(token) > .3:
+                    highSimilarityDict[token] = wordofinterest.similarity(token)
             # The line above creates the structure for each entry in my dictionary.
             # print(token.text, "about this much similar to", wordOfInterest, ": ", wordOfInterest.similarity(token))
-print("This is a dictionary of words most similar to the word " + wordOfInterest.text + " in this file.")
-print(highSimilarityDict)
+        print("This is a dictionary of words most similar to the word " + wordofinterest.text + " in this file.")
+        print(highSimilarityDict)
 
-highSimilarityReduced = {}
-for key, value in highSimilarityDict.items():
-    if value not in highSimilarityReduced.values():
-        highSimilarityReduced[key] = value
+        highSimilarityReduced = {}
+        for key, value in highSimilarityDict.items():
+            if value not in highSimilarityReduced.values():
+                highSimilarityReduced[key] = value
         print(highSimilarityReduced)
-    print(len(highSimilarityReduced.items()), " vs ", len(highSimilarityDict.items()))
+        print(len(highSimilarityReduced.items()), " vs ", len(highSimilarityDict.items()))
+
+        switcheroo = {val: key for key, val in highSimilarityDict.items()}
+        deduped = {val: key for key, val in switcheroo.items()}
+        print(str(len(switcheroo)) + ' **** ' + f'{switcheroo=}')
+
+        print(len(deduped), ' **** ', f'{deduped=}')
+        print(len(deduped.items()), " vs ", len(highSimilarityDict.items()))
+
+# ********* SORTING THE VALUES ************
+# The sorted() function pull the dictionary items out and sorts by value.
+# We set reverse=True so that highest similarity values come out first.
+
+        sortedSimValues = sorted(deduped.items(), key=lambda x: x[1], reverse=True)
+        print(type(sortedSimValues), f'{sortedSimValues=}')
+        sortedSimDict = dict(sortedSimValues)
+        print(type(sortedSimValues), f'{sortedSimValues=}')
+        return sortedSimDict
+# HEY, that's not a dictionary! It's a list. Let's convert it back to a dictionary.
+# ebb: This controls our file handling as a for loop over the directory:
+for file in os.listdir(CollPath):
+    if file.endswith(".txt"):
+        filepath = f"{CollPath}/{file}"
+        print(filepath)
+        readTextFiles(filepath)
